9a0e983f306ccc89213661f37c29c58a20d29e1b
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-08-06 03:50:09 -0400

    created core for the web header

--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import "./web.css";
 
 function Web() {
-  return (
-    <div>
-      This is Web.
+  return <div className="web">
+    <div className="web-option">
+      <a href="#project">
+      <i class="fi-rr-edit option-icon"></i>Projects
+      </a>
     </div>
-  )
+    <div className="web-option">
+      <a href="#skills">
+      <i class="fi-rr-barber-shop option-icon"></i>Skills
+      </a>
+    </div>
+    <div className="web-option">
+      <a href="#work">
+      <i class="fi-rr-briefcase option-icon"></i>Work
+      </a>
+    </div>
+    <div className="web-option">
+      <a href="#contact">
+      <i class="fi-rr-id-badge option-icon"></i>Contact
+      </a>
+    </div>
+  </div>
+  
 }
 
 export default Web
