1578727cd541d7b5c3d290a1d1f72c290f7583a0
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-08-07 04:59:11 -0400

    populated header mobile sidebar with components

--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './mobile.css'
 
-function Mobile() {
+function Mobile({isOpen, setIsOpen}) {
   return (
-    <div>
-      This is Mobile.
+    <div className='mobile'>
+      <div className='close-icon' onClick={()=> setIsOpen(!isOpen)}>
+        <i class="fi-rr-cross"></i>
+      </div>
+      <div className="mobile-options">
+        <div className="mobile-option">
+          <a href="#project">
+          <i class="fi-rr-edit option-icon"></i>Projects
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#skills">
+          <i class="fi-rr-barber-shop option-icon"></i>Skills
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#work">
+          <i class="fi-rr-briefcase option-icon"></i>Work
+          </a>
+        </div>
+        <div className="mobile-option">
+          <a href="#contact">
+          <i class="fi-rr-id-badge option-icon"></i>Contact
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
