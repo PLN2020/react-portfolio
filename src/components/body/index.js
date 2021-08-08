6e99671b9fc502528ce457e2c6ad6b8edf81a879
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-08-08 05:27:37 -0400

    imported all the sections into body/index

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import About from './about/about'
 import './body.css'
+import Contact from './contact/contact'
+import Projects from './projects/projects'
+import Skills from './skills/skills'
+import Work from './work/work'
 
 function Body() {
   return (
     <div className='body'>
-      This is the body.
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="projects">
+        <Projects />
+      </section>
+
+      <section id="skills">
+        <Skills />
+      </section>
+
+      <section id="work">
+        <Work />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
     </div>
   )
 }
