899731e94fc634355c70ee4969c6fd5508f4de9a
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-08-24 01:16:04 -0400

    clicking any item on the mobile sidebar now closes sidebar

--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -8,22 +8,22 @@ function Mobile({isOpen, setIsOpen}) {
         <i class="fi-rr-cross"></i>
       </div>
       <div className="mobile-options">
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={()=> setIsOpen(!isOpen)}>
           <a href="#projects">
           <i class="fi-rr-edit option-icon"></i>Projects
           </a>
         </div>
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={()=> setIsOpen(!isOpen)}>
           <a href="#skills">
           <i class="fi-rr-barber-shop option-icon"></i>Skills
           </a>
         </div>
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={()=> setIsOpen(!isOpen)}>
           <a href="#work">
           <i class="fi-rr-briefcase option-icon"></i>Work
           </a>
         </div>
-        <div className="mobile-option">
+        <div className="mobile-option" onClick={()=> setIsOpen(!isOpen)}>
           <a href="#contact">
           <i class="fi-rr-id-badge option-icon"></i>Contact
           </a>
